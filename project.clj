079f4b76dd7e95b9165cfa11fdacc167ad55171d
detarmstrong <detarmstrong@gmail.com>
2012-08-15 23:30:02 -0700

    Sets the main attribute so that the uberjar can function. Also requires adding :gen-class to the red_jem.core namespace

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                   [seesaw "1.4.1"]
                   [cheshire "4.0.0"]
-                  [clj-http "0.4.1"]])
+                  [clj-http "0.4.1"]]
+  :main red_jem.core)
+
