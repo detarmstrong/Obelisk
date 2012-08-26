b15e9ba982e3d4c7eaa01093d99245caa84450db
detarmstrong <detarmstrong@gmail.com>
2012-08-26 11:34:34 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject red-jem "1.0.0-SNAPSHOT"
+(defproject red-jem "1.0.1"
   :description "Obelisk is a native app for Redmine"
   :dependencies [[org.clojure/clojure "1.3.0"]
                   [seesaw "1.4.1"]
