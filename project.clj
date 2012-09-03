3891b4d5d1328e932d360a04e06721d4fb2d366c
detarmstrong <detarmstrong@gmail.com>
2012-09-03 13:15:12 -0700

    Bump seesaw dependency version Adds dock name jvm opt (not sure this works)

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject red-jem "1.0.1"
   :description "Obelisk is a native app for Redmine"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                  [seesaw "1.4.1"]
+                  [seesaw "1.4.2"]
                   [cheshire "4.0.0"]
                   [clj-http "0.4.1"]]
-  :main red_jem.core)
+  :main red_jem.core
+  :jvm-opts ["-Xdock:name=Obelisk"]) ; this doesn't work consistently
 
