0396f2f9f6a6f4e9dc7ea9d050054a82e6621a37
detarmstrong <detarmstrong@gmail.com>
2012-10-20 14:41:09 -0700

    Commits broken code

--- src/red_jem/virtual_tree.clj
+++ src/red_jem/virtual_tree.clj
@@ -3,7 +3,6 @@ are children of preceeding line."}
      red-jem.virtual-tree)
 
 (declare line-depth)
-(declare current-line-line-depth)
 
 (defn make-tree-data-from-lines
   "Given input of text with leading tabs or spaces that indicate heirarchical depth 
@@ -13,16 +12,27 @@ and tree depth information"
   (loop [lines ws-formatted-lines
          result []]
     (when-let [line (first lines)]
-      (if (children? lines)
-        (recur (children lines)
-               [(clojure.string/trim line)])
-        (conj result (clojure.string/trim line))))))
+      (for [c (children lines)]
+        (recur c [])))))
 
-(defn line-depth [line]
-  "Count the number of whitespaces at beginning of line"
-  (if-not (= line nil)
-  (let [matches (re-find #"^\s*" line)]
-    (count matches))))
+(defn make-tree-root [root children]
+  (conj root (make-tree children)))
+
+(defn make-tree [children]
+  (doseq [child children]
+    (if (parent? child)
+      (doseq [child2 (children child)]
+        (make-tree child2))
+      (reduce ))
+    
+(let [parent-nodes-w-indexes (keep-indexed #(> x y ) children)
+      leaf-nodes-w-indexes (keep-indexed #(= x y) children)
+      result []]
+  (doseq [parent parent-nodes-w-indexes]
+    (make-tree 
+      
+
+; chicken and eggs
 
 (defn children [lines]
   "Return all sublines of the current line of document eg first encountered lines greater
@@ -31,6 +41,21 @@ and tree depth information"
         baseline-depth (line-depth head)]
     (take-while #(> (line-depth %1) baseline-depth) xs)))
 
+(defn siblings [lines]
+  "Skip children of siblings found. Keep indexes of siblings"
+  (let [[head & xs] lines
+        baseline-depth (line-depth head)]
+    (println "siblings of " head)
+    (keep-indexed #(if (= (line-depth %2) baseline-depth)
+                     %2)
+                  (take-while #(>= (line-depth %1) baseline-depth) lines))))
+ 
 (defn children? [lines]
-    (> 0 (count (children lines))))
+    (> (count (children lines)) 0))
+
+(defn line-depth [line]
+  "Count the number of whitespaces at beginning of line"
+  (if-not (= line nil)
+    (let [matches (re-find #"^\s*" line)]
+      (count matches))))
 
