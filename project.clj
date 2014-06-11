82f5ca740d5f82568e15c760c529ae23a94d5a26
detarmstrong <detarmstrong@gmail.com>
2014-06-11 08:34:13 -0700

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject red-jem "1.1.1"
   :description "Obelisk is a native app for Redmine"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                  [seesaw "1.4.2"]
-                  [cheshire "4.0.0"]
-                  [clj-http "0.4.1"]
-                  [org.clojure/core.memoize "0.5.2"]
-                  [org.clojure/tools.trace "0.7.3"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                  [seesaw "1.4.4"]
+                  [cheshire "5.3.1"]
+                  [clj-http "0.9.1"]
+                  [org.clojure/core.memoize "0.5.6"]]
   :main red_jem.core
   :jvm-opts ["-Xdock:name=Obelisk"]) ; this doesn't work consistently
 
