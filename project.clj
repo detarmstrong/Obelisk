f7f007fa089811c224fef527d5abf863cfd36dba
detarmstrong <detarmstrong@gmail.com>
2014-06-22 10:48:17 -0700

    Bump clojure version; add midge

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject red-jem "1.1.1"
   :description "Obelisk is a native app for Redmine"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                   [seesaw "1.4.4"]
                   [cheshire "5.3.1"]
                   [clj-http "0.9.1"]
-                  [org.clojure/core.memoize "0.5.6"]]
+                  [org.clojure/core.memoize "0.5.6"]
+                  [org.clojure/data.xml "0.0.7"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
   :main red_jem.core
   :jvm-opts ["-Xdock:name=Obelisk"]) ; this doesn't work consistently
 
