b7135bab7e3b840e6f2ba2387415a763e16884b1
detarmstrong <detarmstrong@gmail.com>
2014-06-22 10:46:55 -0700

    Copy in whitespace-tree files
    Enables parsing of selected text and visiting nodes in tree

---
+++ src/whitespace_tree/core.clj
@@ -0,0 +1,260 @@
+(ns whitespace-tree.core
+  (:require [clojure.zip :as zip]
+            [clojure.string :as string]
+            [clojure.data.xml :as xml]))
+
+(declare find-by-subject)
+
+(defn sample-text []
+  (slurp "resources/sample-raw-text"))
+
+;===== stackoverflow answer tree parser ====
+(defn tree-text-parser [text]
+  {(re-find #"(?m)^.+" text)
+   (map tree-text-parser
+        (map #(string/replace % #"(?m)^\s{2}" "")
+             (map first (re-seq #"(?m)(^\s{2}.+(\n\s{4}.+$)*)" text))))})
+
+(defn simple-tree-text-to-xml-parser [text]
+  (xml/element :task {:subject (re-find #"(?m)^.+" text)}
+     (map simple-tree-text-to-xml-parser
+            (map #(string/replace % #"(?m)^\s{2}" "")
+                 (map
+                   first
+                   (re-seq #"(?m)(^\s{2}[^\n]+(\n\s{4}.+$)*)" text))))))
+
+(defn simple-tree-text-to-xml-parser2 [text]
+  {:tag :task
+   :attrs {:subject (re-find #"(?m)^.+" text)}
+   :content (let [new-tree (mapv simple-tree-text-to-xml-parser2
+              (map #(string/replace % #"(?m)^\s{2}" "")
+                   (map
+                     first
+                     (re-seq #"(?m)(^\s{2}[^\n]+(\n\s{4}.+$)*)" text))))]
+              (if (= new-tree [])
+                nil
+                new-tree))})
+
+(def description-lines-pattern #"^\s*//.*$")
+(def subject-pattern #"\d*\s*([\s\S]+)$")
+(def leading-id-pattern #"\s*(\d+)\s+")
+
+(defn look-ahead-for-subject-and-description-and-id [text]
+  "Given a whitespacetree, start from first line
+  and identify subject (line that does not start with s*//)
+  and description lines (one or more lines that start with s*//)"
+  (let [lines (string/split text #"(?m)\n")
+        subject (second (re-find subject-pattern (first lines)))
+        id (second (re-find leading-id-pattern (first lines)))
+        description-lines (take-while #(re-find description-lines-pattern %)
+                                      (rest lines))
+        formatted-description-lines (map
+                                      #(string/replace % #"^\s*//\s*" "")
+                                      description-lines)
+        formatted-description (string/join "\n" formatted-description-lines)]
+    {:subject subject
+     :description formatted-description
+     :id id}))
+
+(defn remove-description [text]
+  "format the text so the next description line(s)
+  isn't misinterpreted as a subject"
+  (let [lines (string/split text #"(?m)\n")]
+    (string/join "\n" (cons (first lines)
+                            (drop-while #(re-find description-lines-pattern %)
+                                        (rest lines))))))
+
+(defn format-description-lines [text]
+  "Normalize all description lines (lines matching description-lines-pattern)
+   so that they are indented one width from non-description line above.
+   This is to satisfy the tree-text-to-xml parser so
+   that description lines don't have to be indented in the source text, they can
+   be on the same indent level or less than the subject"
+  (string/join "\n" (reverse
+               (:lines-accum
+                  (reduce (fn [state line]
+                            (let [lines (:lines-accum state)
+                                  stripped-line (string/trim line)
+                                 prev-line (:prev-line state)
+                                 prev-line-indent-width (/ (count (re-find #"^\s*" prev-line)) 2)
+                                 current-line line
+                                 current-indent-width (/ (count (re-find #"^\s*" current-line)) 2)
+                                 prev-line-description-line? (> (count (re-find #"\s*//" prev-line)) 1)
+                                 current-line-description-line? (> (count (re-find #"\s*//" current-line)) 1 )]
+
+                              (if (and (not prev-line-description-line?)
+                                       current-line-description-line?)
+                                ;adjust current line to be one indent greater than previous line
+                                {:prev-line current-line
+                                 :lines-accum (conj lines
+                                                (apply str (reverse (conj (repeat
+                                                             (+ prev-line-indent-width 1) "  ") stripped-line ))))}
+                                {:prev-line current-line
+                                 :lines-accum (conj lines line)})))
+                          {:prev-line "" :lines-accum '()}
+                          (string/split text #"\n"))))))
+
+(defn prepend-rm-id-to-source [result-zipper source-text]
+  "Find line in source-text by finding it in result-zipper. If found
+  prepend id to the line"
+
+  (let [source-text-coll (string/split source-text #"(?m)\n")
+        idified (map #(let [detach-whitespace (re-matches #"(\s*)(.+)" %1)
+                            leading-whitespace (nth detach-whitespace 1)
+                            trimmed-text (nth detach-whitespace 2)
+                            matched-node (find-by-subject result-zipper
+                                                          (clojure.string/trim %1))
+                            id (:id (:attrs matched-node))]
+                        (str leading-whitespace
+                             id
+                             " "
+                             trimmed-text))
+                     source-text-coll)]
+    (string/join "\n" idified)))
+
+(defn next-tree-text-to-xml-parser
+  "This should be called whitespace-tree/parse or parse-to-xml"
+  ([text]
+    (next-tree-text-to-xml-parser
+      (format-description-lines text)
+      true))
+
+  ([text pre-formatted?]
+    (let [subject-and-description (look-ahead-for-subject-and-description-and-id text)
+          subject (:subject subject-and-description)
+          description (let [desc (:description subject-and-description)]
+                        (if (= desc "")
+                          nil
+                          desc))
+          id (:id subject-and-description)
+          new-text (remove-description text)]
+      {:tag :task
+       :attrs {:subject subject :description description :id id}
+       :content (let [new-tree (mapv #(next-tree-text-to-xml-parser % true)
+                        (map #(string/replace % #"(?m)^\s{2}" "")
+                             (map
+                               first
+                               (re-seq #"(?m)(^\s{2}[^\n]+(\n\s{4}.+$)*)" new-text))))]
+                  (if (= new-tree [])
+                    nil
+                    new-tree))})))
+
+(defn my-make-node [existing-node children]
+  (cons (first existing-node) children))
+
+(defn my-zipper [tree]
+  (zip/zipper coll? clojure.core/next my-make-node tree))
+
+;; grokbase tree visitor
+(defn print-tree [loc]
+  (when-not (zip/end? loc)
+    ;; when-not wraps body in implicit do
+    (println
+      (str (string/join "" (repeat (count (zip/path loc)) " "))
+           (if (zip/branch? loc)
+             (first (zip/node loc))
+             (zip/node loc))))
+    (recur (zip/next loc))))
+
+;; puredanger's tree visitor
+(defn visit-node
+  [start-node start-state path visitors]
+  (loop [node start-node
+         state start-state
+         [first-visitor & rest-visitors] visitors]
+    (let [context (merge {:node node, :state state, :stop false, :next false}
+                         (first-visitor node state path))
+          {new-node :node
+           new-state :state
+           ;; :keys is a destructuring directive to specify keys that you would like
+           ;; as locals with the same name
+           :keys (stop next)} context]
+      (if (or next stop (nil? rest-visitors))
+        {:node new-node, :state new-state, :stop stop}
+        (recur new-node new-state rest-visitors)))))
+
+(defn tree-visitor
+  ([zipper visitors]
+   (tree-visitor zipper nil visitors))
+  ([zipper initial-state visitors]
+   (loop [loc zipper
+          state initial-state]
+     (let [context (visit-node (zip/node loc) state (zip/path loc) visitors)
+           new-node (:node context)
+           new-state (:state context)
+           stop (:stop context)
+           new-loc (if (= new-node (zip/node loc))
+                     loc
+                     (zip/replace loc new-node))
+           next-loc (zip/next new-loc)]
+       (if (or (zip/end? next-loc) (= stop true))
+         {:node (zip/root new-loc) :state new-state}
+         (recur next-loc new-state))))))
+
+(defn subject-visitor [node state path]
+  (when (string? (:subject (:attrs node)))
+    {:state (conj state {:subject (:subject (:attrs node))
+                         :path (zip/path node)})}))
+
+(defn subject-collector [node]
+  (:state (tree-visitor node [] [subject-visitor])))
+
+(defn attribute-visitor [attribute node state path]
+  (when (string? (:subject (:attrs node)))
+    {:state (conj state {attribute (attribute (:attrs node))})}))
+
+(defn attribute-collector [node attribute]
+  (:state (tree-visitor node [] [(partial attribute-visitor attribute)])))
+
+(defn matched [subject node state path]
+  (when (= subject (:subject (:attrs node)))
+    {:stop true
+     :state node}))
+
+(defn find-by-subject [node subject]
+  "Finds first node by subject text and returns it"
+  (:state
+    (tree-visitor node [(partial matched subject)])))
+
+(defn parent-visitor [node state path]
+  (when (string? (:subject (:attrs node)))
+    {:state (conj state {:subject (:subject (:attrs node))
+                         :parent (:subject (:attrs (last path)))})}))
+
+(defn leaf-and-parent-collector [node]
+  (:state (tree-visitor node [] [parent-visitor])))
+
+; must make dynamic so I can use (binding) in the midje test to override
+(defn ^:dynamic redmine-ws-post-ticket [subject description parent-id]
+  (println "should be overridden in test"))
+
+(defn redmine-ticket-creator-visitor [creation-service node state path]
+  (if (and (contains? (:attrs node) :id) (number? (:id (:attrs node))))
+    {:state (conj state {:subject (:subject (:attrs node))
+                         :parent (:subject (:attrs (last path)))})}
+    ; create ticket and add id to node
+    (let [parent-id (get-in (first path) [:attrs :id])]
+      {:node (assoc node :attrs (merge (:attrs node) 
+                                       (redmine-ws-post-ticket
+                                         (:subject (:attrs node))
+                                         (:description (:attrs node))
+                                         (:assignee-id (:attrs node))
+                                         (:project-id ?)
+                                         parent-id)))})))
+
+(defn redmine-ticket-tree-transformer [node creation-service]
+  "Run creation-service func on each node"
+  (tree-visitor node [] [(partial redmine-ticket-creator-visitor creation-service)]))
+
+(defn add-assignee-visitor [node state path]
+  ;mutate the node
+  (let [assignee-id (first state)
+        new-state (rest state)]
+    {:node (assoc-in node [:attrs :assignee-id] assignee-id)
+     :state new-state}))
+
+(defn merge-assignees-transformer [node assignments]
+  "Given a list of assignees parallel to assignments, merge into
+   the tree"
+  (:node (tree-visitor node assignments [add-assignee-visitor])))
+
---
+++ test/whitespace_tree/t_core.clj
@@ -0,0 +1,189 @@
+(ns whitespace_tree.t-core
+  (:use midje.sweet)
+  (:use [whitespace_tree.core])
+  (:require [clojure.zip :as zip]
+            [clojure.string :as string]
+            [clojure.data.xml :as xml]
+            [clojure.xml :as xml2]))
+
+(defn make-sequence [init-val]
+  "make a stateful sequence starting at init-val. returns the next number"
+  (let [c (atom init-val)]
+    #(reset! c (+ 1 @c))))
+
+(def seq1 (make-sequence 0))
+
+(defn mock-redmine-post-ticket1 [subject description parent-id]
+  "mock rm ws api for POST of ticket"
+  {:id (seq1) :subject subject :parent-id parent-id})
+
+(def seq2 (make-sequence 0))
+
+(defn mock-redmine-post-ticket2 [subject description parent-id]
+  "mock rm ws api for POST of ticket"
+  {:id (seq2) :subject subject :parent-id parent-id})
+
+(defn sample-text-next []
+  (slurp "resources/sample-raw-text-2.0"))
+
+(defn sample-text-next-xml []
+  (xml2/parse "resources/sample-raw-text-2.0.xml"))
+
+(facts "about SO tree-text-parser"
+  (let [tree-text (sample-text)]
+    (tree-text-parser tree-text)
+    => '{"hierarchy"
+        ({"child 1" ()}
+         {"child 2"
+          ({"child 2.1" ()})}
+         {"child 3"
+          ({"child 3.1" ()})})}))
+
+(facts "about puredanger's visitors: collectors, finders"
+       ; note that we use xml2 parse here because parse defined
+       ; in clojure.data.xml returns strucutre not compatible with xml-zip
+       (let [sample-text (sample-text)
+             generated-xml (xml/emit-str (simple-tree-text-to-xml-parser sample-text))
+             ws-tree-input-stream (java.io.ByteArrayInputStream. (.getBytes generated-xml))
+             tree (zip/xml-zip (xml2/parse ws-tree-input-stream))]
+         (binding [redmine-ws-post-ticket mock-redmine-post-ticket1]
+           (fact "subject-finder returns first node matching subject"
+                  (get-in (find-by-subject tree "child 2")
+                          [:attrs :subject])
+                 => "child 2")
+
+           (fact "subject-collector returns coll of maps"
+                 (reduce #(and %1 (map? %2)) true (subject-collector tree))
+                 => truthy)
+
+           (fact "the 3rd node found by leaf-and-parent-collector has
+                  parent 'child 2'"
+                 (nth (leaf-and-parent-collector tree) 3)
+                 => {:parent "child 2", :subject "child 2.1"})
+
+           (fact "redmine-ticket-tree-transformer sets nil id to 1"
+                 (let [result-tree (redmine-ticket-tree-transformer tree)]
+                   (get-in result-tree [:node :attrs :id])) => 1))))
+
+(facts "about inserting created redmine ids back into source whitespace-tree"
+       (binding [redmine-ws-post-ticket mock-redmine-post-ticket1]
+         (let [source-text (slurp "resources/sample-raw-text")
+               result-tree (zip/xml-zip
+                             (xml2/parse
+                               "resources/sample-raw-text-w-ids.xml"))]
+           (fact "input whitespace tree first line will be prepended by 1"
+                 (prepend-rm-id-to-source result-tree source-text)
+             => "1 hierarchy\n  2 child 1\n  3 child 2\n    4 child 2.1\n  5 child 3\n    6 child 3.1"))))
+
+(facts "about mock-redmine-post-ticket"
+       (fact "mock-redmine-post-ticket will return the next val in sequence"
+             (map #(:id %1)
+                  (repeatedly 6 #(mock-redmine-post-ticket2 "sub" "desc" 1)))
+             => '(1 2 3 4 5 6)))
+
+(facts "about simple-tree-text-to-xml-parser"
+       ; multiroot text selection is "root1\nroot2\n  some child"
+       ; in the above root1 and root2 are really separate trees
+       ;(fact "multiroot tree texts are treated as separated trees per 'root'"
+       ;      false => truthy)
+
+       (fact "input text yields specific xml"
+             (let [sample-text (sample-text)
+                   generated-xml (simple-tree-text-to-xml-parser sample-text)]
+               ;(clojure.pprint/pprint (xml/emit-str generated-xml))
+               (xml/emit-str generated-xml)
+               =>
+               (xml/emit-str (xml/element :task {:subject "hierarchy"}
+                                          (xml/element :task {:subject "child 1"})
+                                          (xml/element :task {:subject "child 2"}
+                                                       (xml/element :task {:subject "child 2.1"}))
+                                          (xml/element :task {:subject "child 3"}
+                                                       (xml/element :task {:subject "child 3.1"})))))))
+
+
+(facts "about next-tree-text-to-xml-parser"
+       (fact "look-ahead-for-subject-and-description-and-id returns subject and description"
+             (let [sample-text (sample-text-next)]
+               (look-ahead-for-subject-and-description-and-id sample-text))
+             =>
+             {:subject "Ticket tree"
+              :description "testing this syntax where description lines are\nstarted with double quote"
+              :id "12"})
+
+       (fact "format-description-lines: initial call of next-tree-text-to-xml-parser
+              will cause all lines beginning with // to be indented
+              one width greater than line above it."
+             (let [sample-text (sample-text-next)]
+               (format-description-lines sample-text) => "12 Ticket tree
+  // testing this syntax where description lines are
+  // started with double quote
+  7832 Already ticketed - improve performance on the ssd
+    //(whitespace after the // is optional) It can't be too fast!
+  rewrite the whole thing
+    // Indented comment for line above.
+    // Second line for comment must not be dedented from comment line above. Rewrite will be profitable and fun \"he said\"
+    rewrite subpart 1
+  another subtask
+    another sub subtask
+  "))
+       ; multiroot text selection is "root1\nroot2\n  some child"
+       ; in the above root1 and root2 are really separate trees
+       ;(fact "multiroot tree texts are treated as separate trees per 'root'"
+           ;  false => truthy)
+       (let [expected-tree-zipper (zip/xml-zip (sample-text-next-xml))
+             sample-text (sample-text-next)
+             under-test-zipper (zip/xml-zip (next-tree-text-to-xml-parser sample-text))]
+         (fact "subject lines are parsed into subject xml attribute"
+               (attribute-collector under-test-zipper :subject)
+               =>
+               (mapv #(let[subj-map %
+                           subj-value (:subject subj-map)
+                           fix-escaping-subj (string/replace
+                                 (or (:subject subj-map) "")
+                                 #"\\n"
+                                 "\n")]
+                        (assoc subj-map
+                               :subject
+                               (if (= "" fix-escaping-subj)
+                                 nil
+                                 fix-escaping-subj)))
+                   (attribute-collector expected-tree-zipper :subject)))
+
+         (fact "id is parsed out of subject lines"
+               (attribute-collector under-test-zipper :id)
+               =>
+               (attribute-collector expected-tree-zipper :id))
+
+         (fact "description lines are parsed into description xml attribute"
+                 (attribute-collector under-test-zipper :description)
+                 =>
+                 (mapv #(let[desc-map %
+                             desc-value (:description desc-map)
+                             fix-escaping-desc (string/replace
+                                   (or (:description desc-map) "")
+                                   #"\\n"
+                                   "\n")]
+                          (assoc desc-map
+                                 :description
+                                 (if (= "" fix-escaping-desc)
+                                   nil
+                                   fix-escaping-desc)))
+                     (attribute-collector expected-tree-zipper :description)))))
+
+(facts "about merge-assignees-transformer"
+       (fact "merge-assignees-transformertakes [assignee-id ...] (parallel vector to subjects) and walks tree"
+             (let [expected-tree-zipper (zip/xml-zip
+                                          (xml2/parse "resources/add-assignee-transformer-pre-filled.xml"))]
+               (merge-assignees-transformer
+                 expected-tree-zipper
+                 [90 90 66 32])
+               =>
+                 {:tag :task
+                  :attrs {:subject "test 1" :assignee-id 90}
+                  :content [{:tag :task
+                             :attrs {:subject "test 2" :assignee-id 90}
+                             :content nil}
+                            {:tag :task
+                             :attrs {:subject "test 3" :assignee-id 66}
+                             :content nil}]})))
+
