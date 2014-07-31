30b5f2335fa47f2783e6e27ce4e93dbad1fb5053
detarmstrong <detarmstrong@gmail.com>
2014-07-30 21:15:29 -0700

    Reset assign page
    Fix #5

--- src/red_jem/events.clj
+++ src/red_jem/events.clj
@@ -14,9 +14,8 @@
 (defn on-create-ticket-form-visible [widget]
   ; only state is inactive
   ; show picker frame
-  (-> options-frame show!) ; not calling pack! because it negates size setting
 
-  (request-focus! widget)
+ ; (request-focus! widget)
   
   ; disable save button
   (config! options-ok-btn :enabled? false)
@@ -24,8 +23,13 @@
   ; load projects in project picker  
   (config! projects-lb :model (projects-listbox-model))
   
-  ; clear members in member picker
-  (config! members-lb :model []))
+  ; reset state of assignees and lines
+  (config! (select options-frame [:#members-panel])
+                 :items [])
+  (config! (select options-frame [:#lines-panel])
+                   :items [])
+  (config! members-lb :model [])
+  (-> options-frame show!)) ; not calling pack! because it negates size setting
 
 (defn on-project-selected [event]
   (if-let [selected-project-id (:id (selection projects-lb))]
@@ -33,14 +37,7 @@
           project-members-as-map (map get-project-member project-members-response)
           project-members-as-map (sort-by :name project-members-as-map)
           selected-text (trimr (get-selected-text area))
-;OK TODO show description lines too in right pane. no dropdown on those lines (see mockup)
-;OK TODO enable ok  button on selection of project
-;TODO hook up the options-ok-btn with saving the tickets
-;TODO give feedback that saving is in process (process includes fail or success paths)
-  ; REDO Mockup: simply prepend animated random rm # then replace with the real number?
-  ; spinner in lower left? 
           lines (clojure.string/split-lines selected-text)
-          ;lines (filter #(empty? (re-matches #"\s*//.*$" %)) lines)
           member-items (conj (mapv (fn [_] (if (empty? (re-matches #"\s*//.*$" _))
                                              (combobox :model project-members-as-map
                                                          :renderer list-renderer
