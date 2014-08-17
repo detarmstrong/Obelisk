d3c0cfac62e59ded393a82b2312dd199c3804256
detarmstrong <detarmstrong@gmail.com>
2014-08-16 18:53:53 -0700

    Set heap size max

--- project.clj
+++ project.clj
@@ -8,5 +8,5 @@
                   [org.clojure/data.xml "0.0.7"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
   :main red_jem.core
-  :jvm-opts ["-Xdock:name=Obelisk"]) ; this doesn't work consistently
+  :jvm-opts ["-Xmx128m"])
 
