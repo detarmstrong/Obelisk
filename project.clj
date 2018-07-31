1508a8b05ee57a911cd600632557951c6d17239c
Danny Armstrong <detarmstrong@dannys-mbp-2.lan>
2018-07-31 12:56:18 -0700

    Use older clj-http to resolve auth error

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                   [seesaw "1.5.0"]
                   [cheshire "5.8.0"]
-                  [clj-http "3.9.0"]
+                  [clj-http "2.2.0"]
                   [org.clojure/core.memoize "0.7.1"]
                   [org.clojure/data.xml "0.0.8"]]
   :profiles {:dev {:dependencies [[midje "1.9.2"]]}}
